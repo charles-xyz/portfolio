ebbe4afd3c6f95c035fcdfe0dabcaf68f4aa81a2
charles-xyz <charleslazaroni@gmail.com>
2024-09-20 17:05:07 -0400

    turning off linting

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { Book, MessageSquare } from 'lucide-react';
 
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function ResumePage() {
     return (
 <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
